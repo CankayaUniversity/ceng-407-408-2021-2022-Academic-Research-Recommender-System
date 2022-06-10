baf9ef6b24488697d644ee6c673ba2a861e1aa3d
CeRN-00 <72130093+CeRN-00@users.noreply.github.com>
2022-06-10 15:03:02 +0300

    Update automated_paper_data.py
    Cleaned

--- DataRelatedWorks/automated_paper_data.py
+++ DataRelatedWorks/automated_paper_data.py
@@ -26,9 +26,6 @@ cursor = connection.cursor()
 
 def paper_crawler():
     existNext=true
-    #nextExist="doesn't matter"
-    #pageNumber=1880 #needs to be taken from database next time ogulcan
-
     crawler_query = "select last_page_num from crawler_last_pagenum"
     cursor.execute(crawler_query)
     pageNumbers = cursor.fetchone()
@@ -43,22 +40,6 @@ def paper_crawler():
     while(existNext==true):
 
         link = "https://paperswithcode.com/api/v1/papers/?page={}&items_per_page=500".format(pageNumber)
-        #older
-        # while(nextExist is not None):
-        #     print(pageNumber)
-
-        #     response = requests.get(link)
-        #     print(response.status_code)
-
-        #     #print(response.content)
-
-        #     jsonResponse = response.json()
-        #     print(len(jsonResponse['results']))
-        #     pageNumber=pageNumber+1
-        #     link = "https://paperswithcode.com/api/v1/papers/?page={}&items_per_page=500".format(pageNumber)
-        #     nextExist=jsonResponse['next']
-        
-        #newer
         response = requests.get(link)
         print(response.status_code)
         jsonResponse = response.json()
@@ -75,8 +56,6 @@ def paper_crawler():
             connection.commit()     
 
         print(len(jsonResponse['results']))
-        
-        #amountOfPaperRange=jsonResponse['count']%500
         amountOfPaperRange=len(jsonResponse['results'])
         fl=1
         insertable_count=1
@@ -86,16 +65,8 @@ def paper_crawler():
             tasks_str=""
             print(fl)
             
-            #print(jsonResponse['next'])
-            #print(jsonResponse['results'][x]['id'])
-            
-
-            
-            #Test whether paper has any tasks if not dont write that paper in database
-            #if(jsonResponse['results'][x]['abstract'] is not None and jsonResponse['results'][x]['abstract']!=''  and jsonResponse['results'][x]['title'] is not None and jsonResponse['results'][x]['title']!='' and jsonResponse['results'][x]['url_abs'] is not None and jsonResponse['results'][x]['url_abs']!='' and jsonResponse['results'][x]['url_pdf'] is not None and jsonResponse['results'][x]['url_pdf']!='' ):
-            if(jsonResponse['results'][x]['abstract'] is not None and jsonResponse['results'][x]['abstract']!=''  and jsonResponse['results'][x]['title'] is not None and jsonResponse['results'][x]['title']!=''):
-
-                link = "https://paperswithcode.com/api/v1/papers/{}/tasks/?page=1&items_per_page=100".format(jsonResponse['results'][x]['id'])
+        if(jsonResponse['results'][x]['abstract'] is not None and jsonResponse['results'][x]['abstract']!=''  and jsonResponse['results'][x]['title'] is not None and jsonResponse['results'][x]['title']!=''):
+                  link = "https://paperswithcode.com/api/v1/papers/{}/tasks/?page=1&items_per_page=100".format(jsonResponse['results'][x]['id'])
                 print(link)
                 response2 = requests.get(link)
                 if(response2.status_code!=404):
@@ -110,14 +81,11 @@ def paper_crawler():
                         for y in range(jsonResponse2['count']):
                             
                             
-                            print(jsonResponse2['results'][y]['id']) #oldu bunu uygula
+                            print(jsonResponse2['results'][y]['id']) 
                             if(y==0):
                                 tasks_str=tasks_str+jsonResponse2['results'][y]['id']
                             else:
                                 tasks_str=tasks_str+","+jsonResponse2['results'][y]['id']
-                            #print(jsonResponse['results'][x]['name'])
-                            #print(jsonResponse['results'][x]['description'])
-
                             print(" ")
                             
                             
@@ -135,10 +103,9 @@ def paper_crawler():
                                     print("{} Doesnt exists in database".format(jsonResponse['results'][x]['id']) )
                                     postgres_insert_query = """ INSERT INTO new_papers_ids (id) VALUES ('{}')""".format(jsonResponse['results'][x]['id'])
                                     record_to_insert = (jsonResponse['results'][x]['id'])
-                                    #cursor.execute(postgres_insert_query, record_to_insert)
                                     cursor.execute(postgres_insert_query)
                                     
-                                    connection.commit() #yoruma aldım ogulcan
+                                    connection.commit() 
                                     print("inserted in newly added papers")
 
                                     print(jsonResponse['results'][x]['published'])
@@ -149,15 +116,13 @@ def paper_crawler():
                                     newAbstract=jsonResponse['results'][x]['abstract']
                                     newAbstract = newAbstract.replace("'"," ")
 
-                                    #papers7 table has 15 columns
                                     if(jsonResponse['results'][x]['published']=='' or jsonResponse['results'][x]['published']==None ):
                                         postgres_insert_query = """ INSERT INTO papers7 (id,title,abstract,authors,pdf_url,github_url,vote_counter,vote_total,view_counter,task_types,done_task,rsponse_fourzrofour,done_tasks_str,done_vec) VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')""".format(jsonResponse['results'][x]['id'], newTitle,newAbstract,authors_array,jsonResponse['results'][x]['url_pdf'],jsonResponse['results'][x]['url_abs'],0,0,0,tasks_str,'TRUE','FALSE','FALSE','FALSE')
                                     else:
                                         postgres_insert_query = """ INSERT INTO papers7 (id,title,abstract,published_date,authors,pdf_url,github_url,vote_counter,vote_total,view_counter,task_types,done_task,rsponse_fourzrofour,done_tasks_str,done_vec) VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')""".format(jsonResponse['results'][x]['id'], newTitle, newAbstract,jsonResponse['results'][x]['published'],authors_array,jsonResponse['results'][x]['url_pdf'],jsonResponse['results'][x]['url_abs'],0,0,0,tasks_str,'TRUE','FALSE','FALSE','FALSE')
                                     
-                                    #record_to_insert = (jsonResponse['results'][x]['id'], jsonResponse['results'][x]['title'], jsonResponse['results'][x]['abstract'],jsonResponse['results'][x]['published'],authors_array,jsonResponse['results'][x]['url_pdf'],jsonResponse['results'][x]['url_abs'],0,0,0,tasks_str,TRUE,FALSE,FALSE,FALSE )
                                     cursor.execute(postgres_insert_query)
-                                    connection.commit() #yoruma aldım ogulcan
+                                    connection.commit()
                                     print("inserted in added papers7")
                                 
 
@@ -168,26 +133,12 @@ def paper_crawler():
                         print(jsonResponse['results'][x]['id']," has zero tasks")
                 elif(response2.status_code==404):
                     print(jsonResponse['results'][x]['id']," 404 404 404")
-
-                        # postgres_insert_query = """ INSERT INTO new_papers_ids (id) VALUES (%s)"""
-                        # record_to_insert = (jsonResponse['results'][x]['id'])
-                        # cursor.execute(postgres_insert_query, record_to_insert)
-                        # connection.commit()
-
-                        #papers6 table has 15 columns
-                        # postgres_insert_query = """ INSERT INTO papers7 (id,title,abstract,published_date,authors,pdf_url,github_url,vote_counter,vote_total,view_counter,task_types,done_task,rsponse_fourzrofour,done_taks_str,done_vec) VALUES (%s,%s,%s,%S,%s,%s,%s,%s,%S,%s,%s,%s,%s,%S,%s)"""
-                        # record_to_insert = (jsonResponse['results'][x]['id'], jsonResponse['results'][x]['name'], jsonResponse['results'][x]['description'], row)
-                        # cursor.execute(postgres_insert_query, record_to_insert) 
             print()
             fl=fl+1
             test_flag=test_flag+1
-            
-            
-            #if(test_flag==500):
-            #    return
+          
         print(jsonResponse['next'])
-        #if(jsonResponse['next'] is None):
-        #    existNext=false         
+     
     print(jsonResponse['next'])        
 
 
@@ -211,16 +162,11 @@ def strToArray():
         print("\n")
 
         postgres_insert_query = """ UPDATE papers7 SET done_tasks_str=true WHERE id='{}'""".format(row[0])
-        #print(postgres_insert_query)
         cursor.execute(postgres_insert_query)
         connection.commit()
 
         for x in list:
-            
-
-            # INSERT INTO papers_tasks(id,task) 
-            # VALUES('test_id','test_task');
-            
+           
             postgres_insert_query = """ INSERT INTO papers_tasks (id, task) VALUES (%s,%s)"""
             record_to_insert = (row[0],x)
             cursor.execute(postgres_insert_query, record_to_insert)
@@ -229,19 +175,6 @@ def strToArray():
             
             
         cnt=cnt+1
-
-
-            # postgres_insert_query = """ INSERT INTO task2 (task_id, task_name, description, area_type) VALUES (%s,%s,%s,%s)"""
-            # record_to_insert = (jsonResponse['results'][x]['id'], jsonResponse['results'][x]['name'], jsonResponse['results'][x]['description'], row)
-            # cursor.execute(postgres_insert_query, record_to_insert)
-
-            # print(x)
-            # print(jsonResponse['results'][x]['id']) #oldu bunu uygula
-            # #print(jsonResponse['results'][x]['name'])
-            # #print(jsonResponse['results'][x]['description']+"\n")
-
-            # connection.commit()
-            # count = cursor.rowcount
 
         print("\n")
 
@@ -255,7 +188,6 @@ def add_task_quantity():
     print(paper_tasks)
     for row in paper_tasks:
         print("\n","!!!",cnt,"!!!")
-        #print(row)
         print(row[0])
         postgreSQL_select_Query_Count = """ select count(*) from papers_tasks where task='{}' """.format(row[0])
         cursor.execute(postgreSQL_select_Query_Count)
@@ -265,12 +197,10 @@ def add_task_quantity():
 
         postgres_insert_query = """ UPDATE task2 SET quantity={} WHERE task_id='{}'""".format(task_Count[0],row[0])
         cursor.execute(postgres_insert_query)
-        #connection.commit()
         print("quantity set")
 
         postgres_insert_query = """ UPDATE task2 SET done_quantity=true WHERE task_id='{}'""".format(row[0])
         cursor.execute(postgres_insert_query)
-        #connection.commit()
         print("done_quantity set")
 
         cnt=cnt+1
@@ -288,11 +218,6 @@ def for_data_query():
         print(row[0])
 
         print(row)
-
-        # postgres_insert_query = """ INSERT INTO tit_abs_vec(id) VALUES (%s)"""
-        # record_to_insert = (row[0])
-        # cursor.execute(postgres_insert_query,record_to_insert)
-        # connection.commit()
 
         postgreSQL_select_Query_Count = """ select title from papers7 where id='{}' """.format(row[0])
         cursor.execute(postgreSQL_select_Query_Count)
@@ -373,48 +298,24 @@ def ssimilarity():
 
                 print(counterForInitValues,"    ",idRow,"    ",counter)
 
-                #postgreSQL_select_Query = "select abstract from papers7 where id='{}'".format(row[0])
-                #cursor.execute(postgreSQL_select_Query)
-                #new_abstract = cursor.fetchone()
-
                 postgreSQL_select_Query = "select titvec from tit_abs_vec where id='{}'".format(row[0])
                 cursor.execute(postgreSQL_select_Query)
                 new_abstract = cursor.fetchone()
-
-            # postgreSQL_select_Query = "select abstract from papers7 where id='{}'".format(idRow[0])
-            # cursor.execute(postgreSQL_select_Query)
-            # compare_abstract = cursor.fetchone()
 
                 postgreSQL_select_Query = "select titvec from tit_abs_vec where id='{}'".format(idRow[0])
                 cursor.execute(postgreSQL_select_Query)
                 compare_abstract = cursor.fetchone()
 
-                #print("new_abstract" + new_abstract[0])
-            # print("yukardaki [} olan allllllllllllllllllllllllllllllllllllllllllllllllo")
-                # test=new_abstract[0]
-                # print("new abtract vectrouuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu: "+test)
-                # print("\n\n\n\n"+"compare abtract vectrouuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu: "+ compare_abstract[0])
-
-                
-                
-
-                # print(type(new_abstract[0]))
-                # print(type(compare_abstract[0]))
+           
+               
                 similarity=util.cos_sim(new_abstract[0],compare_abstract[0])
                 print(similarity)
 
                 print(similarity.numpy()[0])
                 
-
-                #print(type(similarity.numpy()[0]))
-
                 new_sim=float(similarity.numpy()[0])
                 print(new_sim)
-                # print(type(new_sim))
-
-                #query = 'insert into tit_abs_vec(id,titvec,absvec) values (%s, ARRAY [' + vector_title +'],ARRAY [' + vector_abstract +'])'
-                #query = 'insert into cosine_similarity(new_paper_id,compare_paper_id,similarity) values (%s,%s,ARRAY [' + similarity +'])'
-
+              
 
                 query = 'insert into cosine_similarity(new_paper_id,compare_paper_id,similarity) values (%s,%s,%s)'
                 record_to_insert=(row[0],idRow[0],new_sim)
@@ -430,7 +331,6 @@ def ssimilarity():
         connection.commit()
 
         counterForInitValues=counterForInitValues+1
-            #print("similarity:", util.cos_sim(query_embedding,passage_embedding))
         cnt=cnt+1
 
 
